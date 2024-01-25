569e48c92445060f84d7ac7f826068612398fd2d
alex <a8696tk@gmail.com>
2024-01-25 16:16:12 +0200

    add convert function

--- home_study.py
+++ home_study.py
@@ -1,15 +1,26 @@
 # arithmetic mean
-result = 0
-count = 0
-temp_var = 0
+# result = 0
+# count = 0
+# temp_var = 0
+#
+# def mean(*digit):
+#     global temp_var, count, result
+#     for num in digit:
+#         temp_var += num
+#         count += 1
+#     result += temp_var / count
+#     return result
+#
+#
+# print(round(mean(2, 3, 4, 5, 6, 7, 8, 9, 9, 9, 8, 7, 6, 7, 8, 7, 7, 7, 8, 8, 9, 8, 8), 2))
 
-def mean(*digit):
-    global temp_var, count, result
-    for num in digit:
-        temp_var += num
-        count += 1
-    result += temp_var / count
-    return result
+# перевод из десятичной в двоичную систему
+def convert_num(number):
+    if number > 0:
+        return number % 2 + 10 * convert_num(number // 2)
+    else:
+        return 0
 
 
-print(round(mean(2, 3, 4, 5, 6, 7, 8, 9, 9, 9, 8, 7, 6, 7, 8, 7, 7, 7, 8, 8, 9, 8, 8), 2))
+num = int(input("Enter a number: "))
+print(convert_num(num))
