c48b99bfebfd5a85099ca3cd6e5180d762e78618
alex <a8696tk@gmail.com>
2024-02-08 15:00:14 +0200

    add function sum_numbers and three options solutions

--- py_checkio_org_tasks.py
+++ py_checkio_org_tasks.py
@@ -57,30 +57,74 @@
 """
 
 
-def most_frequent(data: list[str]) -> str:
-    # most_common = None
-    # counter = 0
-    #
-    # for item in data:
-    #     a = data.count(item)
-    #     if a > counter:
-    #         most_common = item
-    #         counter += 1
-    #
-    # return most_common
+# def most_frequent(data: list[str]) -> str:
+#     # most_common = None
+#     # counter = 0
+#     #
+#     # for item in data:
+#     #     a = data.count(item)
+#     #     if a > counter:
+#     #         most_common = item
+#     #         counter += 1
+#     #
+#     # return most_common
+#
+#     # or
+#     return max(data, key=data.count)
+#
+#
+# print("Example:")
+# print(most_frequent(["a", "b", "c", "a", "b", "a"]))
+#
+# # These "asserts" are used for self-checking
+# assert most_frequent(['a', 'b', 'c', 'a', 'b', 'a']) == 'a'
+# assert most_frequent(['a', 'a', 'bi', 'bi', 'bi']) == 'bi'
+# assert most_frequent(['a']) == 'a'
+# assert most_frequent(['a', 'a']) == 'a'
+# assert most_frequent(['a', 'a', 'z']) == 'a'
+#
+# print("The mission is done! Click 'Check Solution' to earn rewards!")
 
-    # or
-    return max(data, key=data.count)
+"""
+    У заданому тексті тобі потрібно підсумувати числа, виключивши будь-які цифри,
+     які є частиною слова.
+    Текст складається з чисел, пробілів та літер з англійського алфавіту.
+"""
 
 
-print("Example:")
-print(most_frequent(["a", "b", "c", "a", "b", "a"]))
+# def sum_numbers(text: str) -> int:
+#     res = 0
+#     is_digit = text.split()
+#     if len(is_digit) == 0:
+#         res = 0
+#     else:
+#         for num in is_digit:
+#             if num.isdigit():
+#                 res += int(num)
+#     return res
+#
+# # or
+# # sum_numbers = lambda text: sum(int(word) for word in text.split() if word.isdigit())
+#
+# ## or
+# ## def sum_numbers(text: str) -> int:
+#     return sum(map(int, filter(str.isdigit, text.split())))
+#
+# print("Example:")
+# print(sum_numbers("hi"))
+#
+# # These "asserts" are used for self-checking
+# assert sum_numbers("hi") == 0
+# assert sum_numbers("who is 1st here") == 0
+# assert sum_numbers("my numbers is 2") == 2
+# assert (
+#     sum_numbers(
+#         "This picture is an oil on canvas painting by Danish artist Anna Petersen between 1845 and 1910 year"
+#     )
+#     == 3755
+# )
+# assert sum_numbers("5 plus 6 is") == 11
+# assert sum_numbers("") == 0
+#
+# print("The mission is done! Click 'Check Solution' to earn rewards!")
 
-# These "asserts" are used for self-checking
-assert most_frequent(['a', 'b', 'c', 'a', 'b', 'a']) == 'a'
-assert most_frequent(['a', 'a', 'bi', 'bi', 'bi']) == 'bi'
-assert most_frequent(['a']) == 'a'
-assert most_frequent(['a', 'a']) == 'a'
-assert most_frequent(['a', 'a', 'z']) == 'a'
-
-print("The mission is done! Click 'Check Solution' to earn rewards!")
