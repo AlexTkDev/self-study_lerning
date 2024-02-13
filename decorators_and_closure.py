9215ad8ac2e8a5cd9360f868740c95644d7b3ce2
alex <a8696tk@gmail.com>
2024-02-13 10:22:45 +0200

    add time_execution_func

--- decorators_and_closure.py
+++ decorators_and_closure.py
@@ -1,52 +1,56 @@
-"""
-Здесь мы создаём декоратор, замеряющий время выполнения функции.
-Далее мы используем его на функции, которая делает GET-запрос к главной странице Google.
-Чтобы измерить скорость, мы сначала сохраняем время перед выполнением обёрнутой функции,
-выполняем её, снова сохраняем текущее время и вычитаем из него начальное.
-"""
-
-
-# def benchmark(func):
-#     import time
+# import functools
+# import time
 #
-#     def wrapper():
+#
+# def benchmark(func):
+#     """Декоратор, замеряющий время выполнения функции.
+#     Далее мы используем его на функции, которая делает GET-запрос к главной странице Google.
+#     Чтобы измерить скорость, мы сначала сохраняем время перед выполнением обёрнутой функции,
+#     выполняем её, снова сохраняем текущее время и вычитаем из него начальное."""
+#
+#     def wrapper(*args, **kwargs):
 #         start = time.time()
-#         func()
+#         return_value = func(*args, **kwargs)
 #         end = time.time()
 #         print('[*] Время выполнения: {} секунд.'.format(end - start))
-#
+#         return return_value
 #     return wrapper
 #
 #
 # @benchmark
-# def fetch_webpage():
+# def fetch_webpage(url):
 #     import requests
-#     webpage = requests.get('https://google.com')
+#     webpage = requests.get(url)
+#     return webpage.status_code, webpage.url
 #
 #
-# fetch_webpage()
-
-#   ------ Более продвинутый вариант ----------
-
-def benchmark(func):
-    import time
-
-    def wrapper(*args, **kwargs):
-        start = time.time()
-        return_value = func(*args, **kwargs)
-        end = time.time()
-        print('[*] Время выполнения: {} секунд.'.format(end - start))
-        return return_value
-
-    return wrapper
+# webpage = fetch_webpage('https://google.com')
+# print(webpage)
 
 
-@benchmark
-def fetch_webpage(url):
-    import requests
-    webpage = requests.get(url)
-    return webpage.status_code, webpage.url
+# ------------------------------------------------------------
 
-
-webpage = fetch_webpage('https://google.com')
-print(webpage)
+# import functools
+# import time
+#
+#
+# def time_execution_func(func):
+#     """Этот декоратор принимает функцию и
+#      просчитывает время её выполнения"""
+#
+#     @functools.wraps(func)
+#     def wrapper(*args, **kwargs):
+#         start_time = time.time()
+#         result = func(*args, **kwargs)
+#         print(f"""Время выполнения функции {func} = {time.time() - start_time}""")
+#         return result
+#
+#     return wrapper
+#
+#
+# @time_execution_func
+# def power(a, b):
+#     return a ** b
+#
+#
+# print(power(2, 18))
