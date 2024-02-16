b7ee0999bdc07def00fbd61c8daeccdfacd30d90
alex <a8696tk@gmail.com>
2024-02-16 10:56:43 +0200

    add function duplicate_zeros

--- py_checkio_org_tasks.py
+++ py_checkio_org_tasks.py
@@ -343,3 +343,41 @@
 # assert nearest_value({5}, 5) == 5
 # assert nearest_value({5}, 7) == 5
 # print("The mission is done! Click 'Check Solution' to earn rewards!")
+
+
+from collections.abc import Iterable
+
+
+def duplicate_zeros(donuts: list[int]) -> Iterable[int]:
+    result = []
+    for key in donuts:
+        if key == 0:
+            a_temp = [int(i) for i in str(key) * 2]
+            result.append(a_temp[0])
+            result.append(a_temp[1])
+        else:
+            result.append(key)
+
+    return result
+
+
+print("Example:")
+print(list(duplicate_zeros([1, 0, 2, 3, 0, 4, 5, 0])))
+#
+# # These "asserts" are used for self-checking
+assert list(duplicate_zeros([1, 0, 2, 3, 0, 4, 5, 0])) == [
+    1,
+    0,
+    0,
+    2,
+    3,
+    0,
+    0,
+    4,
+    5,
+    0,
+    0,
+]
+assert list(duplicate_zeros([0, 0, 0, 0])) == [0, 0, 0, 0, 0, 0, 0, 0]
+assert list(duplicate_zeros([100, 10, 0, 101, 1000])) == [100, 10, 0, 0, 101, 1000]
+print("The mission is done! Click 'Check Solution' to earn rewards!")
