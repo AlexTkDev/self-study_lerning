0af69b9c5897c4fcb033c06db78f499ad49f381a
alex <a8696tk@gmail.com>
2024-02-16 11:16:04 +0200

    add more solution options

--- py_checkio_org_tasks.py
+++ py_checkio_org_tasks.py
@@ -13,8 +13,7 @@
 # assert first_word("hi") == "hi"
 #
 # print("The mission is done! Click 'Check Solution' to earn rewards!")
-
-
+import itertools
 # def is_even(num: int) -> bool:
 #     # your code here
 #     return num & 1 == 0
@@ -345,39 +344,51 @@
 # print("The mission is done! Click 'Check Solution' to earn rewards!")
 
 
-from collections.abc import Iterable
-
-
-def duplicate_zeros(donuts: list[int]) -> Iterable[int]:
-    result = []
-    for key in donuts:
-        if key == 0:
-            a_temp = [int(i) for i in str(key) * 2]
-            result.append(a_temp[0])
-            result.append(a_temp[1])
-        else:
-            result.append(key)
-
-    return result
-
-
-print("Example:")
-print(list(duplicate_zeros([1, 0, 2, 3, 0, 4, 5, 0])))
+# from collections.abc import Iterable
 #
-# # These "asserts" are used for self-checking
-assert list(duplicate_zeros([1, 0, 2, 3, 0, 4, 5, 0])) == [
-    1,
-    0,
-    0,
-    2,
-    3,
-    0,
-    0,
-    4,
-    5,
-    0,
-    0,
-]
-assert list(duplicate_zeros([0, 0, 0, 0])) == [0, 0, 0, 0, 0, 0, 0, 0]
-assert list(duplicate_zeros([100, 10, 0, 101, 1000])) == [100, 10, 0, 0, 101, 1000]
-print("The mission is done! Click 'Check Solution' to earn rewards!")
+#
+# def duplicate_zeros(donuts: list[int]) -> Iterable[int]:
+#     # result = []
+#     # for key in donuts:
+#     #     if key == 0:
+#     #         a_temp = [int(i) for i in str(key) * 2]
+#     #         result.append(a_temp[0])
+#     #         result.append(a_temp[1])
+#     #     else:
+#     #         result.append(key)
+#     #
+#     # return result
+#
+#     #or
+#     # return sum([[i] if i else [0, 0] for i in donuts], [])
+#
+#     # or
+#     for donut in donuts:
+#         if donut == 0:
+#             yield 0
+#         yield donut
+#     return []
+#
+#
+# print("Example:")
+# print(list(duplicate_zeros([1, 0, 2, 3, 0, 4, 5, 0])))
+# #
+# # # These "asserts" are used for self-checking
+# assert list(duplicate_zeros([1, 0, 2, 3, 0, 4, 5, 0])) == [
+#     1,
+#     0,
+#     0,
+#     2,
+#     3,
+#     0,
+#     0,
+#     4,
+#     5,
+#     0,
+#     0,
+# ]
+# assert list(duplicate_zeros([0, 0, 0, 0])) == [0, 0, 0, 0, 0, 0, 0, 0]
+# assert list(duplicate_zeros([100, 10, 0, 101, 1000])) == [100, 10, 0, 0, 101, 1000]
+# print("The mission is done! Click 'Check Solution' to earn rewards!")
+
+
